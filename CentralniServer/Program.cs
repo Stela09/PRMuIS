be806d9318e1f2e1af1d40dcbea67da5273a45ac
obren123 <obrentanasic@gmail.com>
2025-09-19 02:36:31 +0200

    Multiplekser i skice

--- CentralniServer/Program.cs
+++ CentralniServer/Program.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-using Domen.Modeli;
 using Domen;
+using Domen.Modeli;
+
 namespace Server
 {
-    internal class CentralServerApp
+    internal class CentralServerNonBlocking
     {
         private const int LISTEN_PORT = 10000;
         private static Socket _listenerSocket;
@@ -16,7 +17,7 @@ namespace Server
         private static Dictionary<Socket, Stanica> _mapaStanica = new Dictionary<Socket, Stanica>();
         private static readonly Random _rand = new Random();
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             InicijalizujServer();
             PokreniServer();
@@ -29,6 +30,8 @@ namespace Server
                 _listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 _listenerSocket.Bind(new IPEndPoint(IPAddress.Any, LISTEN_PORT));
                 _listenerSocket.Listen(10);
+                _listenerSocket.Blocking = false;
+
                 Console.WriteLine($"Centralni server pokrenut na portu {LISTEN_PORT}");
             }
             catch (Exception ex)
@@ -40,146 +43,118 @@ namespace Server
 
         private static void PokreniServer()
         {
-            // Pokreni thread za prihvat stanica
-            var listenerThread = new Thread(ObradiDolazneStanice);
-            listenerThread.Start();
-
-            // Pokreni prikaz statusa mreže
-            var displayThread = new Thread(PrikaziStatusMreze);
-            displayThread.Start();
-
-            Console.WriteLine("Pritisnite Enter za izlaz...");
-            Console.ReadLine();
-
-            foreach (var socket in _povezaneStanice)
-            {
-                socket?.Close();
-            }
-            _listenerSocket?.Close();
-        }
-
-        private static void ObradiDolazneStanice()
-        {
             string[] gradovi = { "Beograd", "Novi Sad", "Niš", "Kragujevac", "Subotica" };
 
             while (true)
             {
-                try
+                // Prihvatanje novih stanica
+                if (_listenerSocket.Poll(500 * 1000, SelectMode.SelectRead))
                 {
-                    Socket stanicaSocket = _listenerSocket.Accept();
-
-                    // Generiši početne podatke stanice
-                    var stanica = new Stanica
+                    try
                     {
-                        Naziv = gradovi[_povezaneStanice.Count % gradovi.Length],
-                        Koordinate = new Koordinate(
-                            42.0 + _rand.NextDouble() * 3.0,   // širina
-                            19.0 + _rand.NextDouble() * 5.0    // dužina
-                        ),
-                        BrojStanovnika = _rand.Next(50000, 2000000),
-                        BrojUredjaja = _rand.Next(3, 10)
-                    };
+                        Socket s = _listenerSocket.Accept();
+                        s.Blocking = false;
 
-                    // Pošalji početne podatke stanice
-                    NetworkHelper.SendMessage(stanicaSocket, stanica);
-
-                    lock (_mapaStanica)
-                    {
-                        _povezaneStanice.Add(stanicaSocket);
-                        _mapaStanica.Add(stanicaSocket, stanica);
-                    }
-
-                    // Pokreni thread za primanje podataka sa stanice
-                    var stanicaThread = new Thread(() => PrimiPodatkeStanice(stanicaSocket));
-                    stanicaThread.Start();
-
-                    Console.WriteLine($"Nova stanica povezana: {stanica.Naziv}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Greška prilikom prihvata stanice: {ex.Message}");
-                }
-            }
-        }
-
-        private static void PrimiPodatkeStanice(Socket stanicaSocket)
-        {
-            while (true)
-            {
-                try
-                {
-                    var azuriranaStanica = NetworkHelper.ReceiveMessage<Stanica>(stanicaSocket);
-
-                    lock (_mapaStanica)
-                    {
-                        if (_mapaStanica.ContainsKey(stanicaSocket))
+                        var stanica = new Stanica
                         {
-                            // Dodaj nova merenja u postojeću listu
-                            _mapaStanica[stanicaSocket].Merenja.AddRange(azuriranaStanica.Merenja);
-                            _mapaStanica[stanicaSocket].AktivniAlarmi.AddRange(azuriranaStanica.AktivniAlarmi);
-                        }
-                        else
+                            Naziv = gradovi[_povezaneStanice.Count % gradovi.Length],
+                            Koordinate = new Koordinate(
+                                42.0 + _rand.NextDouble() * 3.0,
+                                19.0 + _rand.NextDouble() * 5.0
+                            ),
+                            BrojStanovnika = _rand.Next(50000, 2000000),
+                            BrojUredjaja = _rand.Next(3, 10)
+                        };
+
+                        _povezaneStanice.Add(s);
+                        _mapaStanica[s] = stanica;
+
+                        // Pošalji početne podatke stanice preko NetworkHelper
+                        NetworkHelper.SendMessage(s, stanica);
+
+                        Console.WriteLine($"Nova stanica povezana: {stanica.Naziv}");
+                    }
+                    catch (SocketException)
+                    {
+                        // Nema novih konekcija
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Greška prilikom prihvata stanice: {ex.Message}");
+                    }
+                }
+
+                // Primanje podataka sa stanica
+                foreach (var s in _povezaneStanice.ToList())
+                {
+                    try
+                    {
+                        if (s.Poll(500 * 1000, SelectMode.SelectRead))
                         {
-                            // Nova stanica
-                            _mapaStanica[stanicaSocket] = azuriranaStanica;
+                            // Koristimo NetworkHelper da primimo kompletan objekat Stanica
+                            Stanica azuriranaStanica = NetworkHelper.ReceiveMessage<Stanica>(s);
+
+                            // Dodaj nova merenja i alarme u postojeću stanicu
+                            const int MAX_MERENJA = 3;
+
+                            var merenja = _mapaStanica[s].Merenja;
+                            merenja.AddRange(azuriranaStanica.Merenja);
+
+                            // Ograniči listu na poslednjih MAX_MERENJA
+                            if (merenja.Count > MAX_MERENJA)
+                            {
+                                int removeCount = merenja.Count - MAX_MERENJA;
+                                merenja.RemoveRange(0, removeCount);
+                            }
+                            _mapaStanica[s].AktivniAlarmi.AddRange(azuriranaStanica.AktivniAlarmi);
                         }
                     }
-
+                    catch
+                    {
+                        Console.WriteLine($"Stanica {_mapaStanica[s].Naziv} se odjavila.");
+                        _mapaStanica.Remove(s);
+                        _povezaneStanice.Remove(s);
+                        s.Close();
+                    }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Greška prilikom primanja podataka stanice: {ex.Message}");
-                    break;
-                }
-            }
 
-            // Čišćenje nakon odjave stanice
-            lock (_mapaStanica)
-            {
-                _mapaStanica.Remove(stanicaSocket);
-                _povezaneStanice.Remove(stanicaSocket);
+                // Prikaz statusa mreže
+                PrikaziStatusMreze();
+
+                Thread.Sleep(500); // osvežavanje svakog sekunda
             }
-            stanicaSocket.Close();
         }
 
         private static void PrikaziStatusMreze()
         {
-            while (true)
+            Console.Clear();
+            Console.WriteLine("\n=== STATUS METEOROLOŠKE MREŽE ===\n");
+            Console.WriteLine($"Aktivne stanice: {_mapaStanica.Count}\n");
+
+            foreach (var stanica in _mapaStanica.Values)
             {
-                Console.Clear();
-                Console.WriteLine("\n=== STATUS METEOROLOŠKE MREŽE ===\n");
-                Console.WriteLine($"Aktivne stanice: {_mapaStanica.Count}\n");
+                Console.WriteLine($"=== {stanica.Naziv} ===");
+                Console.WriteLine($"Lokacija: {stanica.Koordinate.Sirina:F2}°N, {stanica.Koordinate.Duzina:F2}°E");
+                Console.WriteLine($"Broj stanovnika: {stanica.BrojStanovnika:N0}");
+                Console.WriteLine($"Broj uređaja: {stanica.BrojUredjaja}");
+                Console.WriteLine("\nNedavna merenja:");
+                Console.WriteLine("Uređaj\tTip\tVrednost\tJedinica\tVreme");
 
-                lock (_mapaStanica)
+                foreach (var m in stanica.Merenja)
                 {
-                    foreach (var stanica in _mapaStanica.Values)
-                    {
-                        Console.WriteLine($"=== {stanica.Naziv} ===");
-                        Console.WriteLine($"Lokacija: {stanica.Koordinate.Sirina:F2}°N, {stanica.Koordinate.Duzina:F2}°E");
-                        Console.WriteLine($"Broj stanovnika: {stanica.BrojStanovnika:N0}");
-                        Console.WriteLine($"Broj uređaja: {stanica.BrojUredjaja}");
-                        Console.WriteLine("\nNedavna merenja:");
-                        Console.WriteLine("Uređaj\tTip\tVrednost\tJedinica\tVreme");
-
-                        foreach (var m in stanica.Merenja)
-                        {
-                            Console.WriteLine($"{m.IdUredjaja}\t{m.Tip}\t{m.Vrednost:F1}\t{m.Jedinica}\t{m.Vreme:HH:mm:ss}");
-                        }
-
-                        var poslednjiAlarm = stanica.AktivniAlarmi.LastOrDefault();
-                        if (poslednjiAlarm != null)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("\n!!! POSLEDNJI AKTIVNI ALARM !!!");
-                            Console.WriteLine($"[{poslednjiAlarm.Vreme:HH:mm:ss}] {poslednjiAlarm.Uzrok} ({poslednjiAlarm.Vrednost})");
-                            Console.ResetColor();
-                        }
-
-                        Console.WriteLine();
-                    }
+                    Console.WriteLine($"{m.IdUredjaja}\t{m.Tip}\t{m.Vrednost:F1}\t{m.Jedinica}\t\t{m.Vreme:HH:mm:ss}");
                 }
 
-                Thread.Sleep(1000); // osvežavanje svakog sekunda
+                var poslednjiAlarm = stanica.AktivniAlarmi.LastOrDefault();
+                if (poslednjiAlarm != null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n!!! POSLEDNJI AKTIVNI ALARM !!!");
+                    Console.WriteLine($"[{poslednjiAlarm.Vreme:HH:mm:ss}] {poslednjiAlarm.Uzrok} ({poslednjiAlarm.Vrednost})");
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine();
             }
         }
     }
